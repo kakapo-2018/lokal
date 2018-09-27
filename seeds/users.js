ea2b7de9c13847f28edfa367ba9225a79f85c0cf
Nepcam <cameron.nepe@gmail.com>
2018-09-27 19:06:00 +1200

    tweaked seeds

--- seeds/users.js
+++ seeds/users.js
@@ -7,7 +7,8 @@ exports.seed = function(knex, Promise) {
       return knex('users').insert([
         {id: 1, iwi_name: 'Te Āti Awa', location: 'Wellington', contact_name: 'Aroha', email: 'aroha@123.com', phone: 1234, hash: ''},
         {id: 2, iwi_name: 'Ngāti Toa', location: 'Porirua', contact_name: 'Tama', email: 'tama@123.com', phone: 1234, hash: ''},
-        {id: 3, iwi_name: 'Te Arawa', location: 'Porirua', contact_name: 'Tama', email: 'tama@123.com', phone: 1234, hash: ''}
+        {id: 3, iwi_name: 'Te Arawa', location: 'Tauranga', contact_name: 'Tama', email: 'tama@123.com', phone: 1234, hash: ''},
+        {id: 4, iwi_name: 'Taranaki ki Te Upoko o Te Ika', location: 'Wellington', contact_name: 'Tama', email: 'tama@123.com', phone: 1234, hash: ''}
       ]);
     });
 };

0593164f89724ad7871b8fa7b5115008c33741ce
ben-vandelaar <benvandelaar@gmail.com>
2018-09-27 17:26:43 +1200

    sotries

--- seeds/users.js
+++ seeds/users.js
@@ -6,7 +6,8 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('users').insert([
         {id: 1, iwi_name: 'Te Āti Awa', location: 'Wellington', contact_name: 'Aroha', email: 'aroha@123.com', phone: 1234, hash: ''},
-        {id: 2, iwi_name: 'Ngāti Toa', location: 'Porirua', contact_name: 'Tama', email: 'tama@123.com', phone: 1234, hash: ''}
+        {id: 2, iwi_name: 'Ngāti Toa', location: 'Porirua', contact_name: 'Tama', email: 'tama@123.com', phone: 1234, hash: ''},
+        {id: 3, iwi_name: 'Te Arawa', location: 'Porirua', contact_name: 'Tama', email: 'tama@123.com', phone: 1234, hash: ''}
       ]);
     });
 };

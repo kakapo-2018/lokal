e98d974b503c62baf287f551b24911f52c392ad1
Nepcam <cameron.nepe@gmail.com>
2018-10-03 16:23:45 +1300

    dev working with all models

--- client/components/Addedit.jsx
+++ client/components/Addedit.jsx
@@ -60,7 +60,7 @@ class Addedit extends React.Component {
                   <span class="input-group-text">Upload Image</span>
                 </div>
                 <div class="custom-file">
-                  <input type="file" class="custom-file-input" value={this.state.iwiStory.image} name="image" onChange={this.handleChange}/>
+                  <input type="file" class="custom-file-input" name="image" onChange={this.handleChange}/>
                   <label class="custom-file-label" for="inputGroupFile01">
                     Choose Image
                   </label>
@@ -72,7 +72,7 @@ class Addedit extends React.Component {
                   <span class="input-group-text">Upload Audio</span>
                 </div>
                 <div class="custom-file">
-                  <input type="file" class="custom-file-input" value={this.state.iwiStory.obj} name="obj" onChange={this.handleChange}/>
+                  <input type="file" class="custom-file-input" name="obj" onChange={this.handleChange}/>
                   <label class="custom-file-label" for="inputGroupFile01">
                     Choose Audio
                   </label>
